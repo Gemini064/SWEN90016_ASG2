6cf83c15a68871957e331ff61ae654452ad30669
juliayanggg <chuchuny@student.unimelb.edu.au>
2021-09-14 15:48:58 +0800

    add style

--- page-login.php
+++ page-login.php
@@ -37,7 +37,7 @@
 			$error_flag = 0;
 		}
 	}	
-?>
+?> 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -46,6 +46,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=, initial-scale=1.0">
 		<title>Login</title>
+		<link rel="stylesheet" href="style.css">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	</head>
 	<body>		
--- page-register.php
+++ page-register.php
@@ -23,7 +23,7 @@
 	// }
 	
 	// unset($_SESSION['decide']);
-?>
+?> 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -32,6 +32,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=, initial-scale=1.0">
 		<title>Sign Up</title>
+		<link rel="stylesheet" href="style.css">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	</head>
 	<body>		
